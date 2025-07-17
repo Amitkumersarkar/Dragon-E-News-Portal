dd1998e8551a79a67ef08605f6708536a042983f
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-07-17 23:33:11 +0600

    added marquee animation

--- src/Components/LatestNews.jsx
+++ src/Components/LatestNews.jsx
@@ -1,10 +1,25 @@
+import Marquee from "react-fast-marquee";
+import { Link } from "react-router-dom";
 
 const LatestNews = () => {
 
     return (
-        <div className="flex gap-3 items-center">
+        <div className="flex gap-3 items-center bg-gray-300">
             <p className="bg-[#D72050] text-white font-semibold p-2">Latest</p>
-            <p>Lorem, ipsum dolor sit consectetur adipisicing elit. Incidunt dolorum praesentium a, molestiae itaque quia nobis temporibus quaerat.</p>
+            <Marquee pauseOnHover={true} className="space-x-10 ">
+                <Link to='/news'>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, sunt.
+                </Link>
+                <Link to='/news'>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, sunt.
+                </Link>
+                <Link to='/news'>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, sunt.
+                </Link>
+                <Link to='/news'>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, sunt.
+                </Link>
+            </Marquee>
         </div>
     );
 };
