b1d6baab5de5138ac043557e8329307e55b91999
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-07-18 00:40:41 +0600

    added sidebar

--- src/Layouts/HomePages/Home.jsx
+++ src/Layouts/HomePages/Home.jsx
@@ -12,14 +12,13 @@ const Home = () => {
                     <LatestNews></LatestNews>
                 </section>
             </header>
-            <section className="w-11/12 mx-auto py-3">
+            <nav className="w-11/12 mx-auto py-3">
                 <Navbar></Navbar>
-            </section>
-            <nav>
-
             </nav>
-            <main>
-
+            <main className="w-11/12 mx-auto pt-5 grid md:grid-cols-12 gap-3">
+                <aside className="left col-span-3">left sidebar</aside>
+                <section className="col-span-6">main content</section>
+                <aside className="col-span-3">right sidebar</aside>
             </main>
         </div>
     );
