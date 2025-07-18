a6aa1d0a308cf0518e754cbeb76730db17fbb704
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-07-18 12:47:04 +0600

    added categories section

--- src/Layouts/HomePages/Home.jsx
+++ src/Layouts/HomePages/Home.jsx
@@ -1,6 +1,7 @@
 import Header from "../../Components/Header/Header";
 import Navbar from "../../Components/Header/Navbar";
 import LatestNews from "../../Components/LatestNews";
+import LeftNavbar from "../LeftNavbar";
 
 const Home = () => {
 
@@ -16,7 +17,9 @@ const Home = () => {
                 <Navbar></Navbar>
             </nav>
             <main className="w-11/12 mx-auto pt-5 grid md:grid-cols-12 gap-3">
-                <aside className="left col-span-3">left sidebar</aside>
+                <aside className="left col-span-3">
+                    <LeftNavbar></LeftNavbar>
+                </aside>
                 <section className="col-span-6">main content</section>
                 <aside className="col-span-3">right sidebar</aside>
             </main>
