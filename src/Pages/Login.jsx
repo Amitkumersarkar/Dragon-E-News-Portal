7f4985f1e9411beb3bf7b0757b8e8f31dc1ece4f
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-07-20 23:30:39 +0600

    updated file

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,7 +1,15 @@
 import { Link } from "react-router-dom";
 
 const Login = () => {
+    const handleSubmit2 = (e) => {
+        e.preventDefault();
+        // get form data here
+        const form = new FormData(e.target);
+        const email = form.get('email');
+        const password = form.get('password');
+        console.log(email, password);
 
+    }
     return (
         <div className="bg-base-200 min-h-screen flex items-start pt-10 px-4">
             <div className="flex flex-col items-center w-full max-w-md mx-auto space-y-5">
@@ -16,25 +24,27 @@ const Login = () => {
 
                 {/* Login Form */}
                 <div className="card bg-base-100 w-full shadow-2xl">
-                    <div className="card-body">
-                        <fieldset className="fieldset space-y-4">
-                            <div>
-                                <label className="label text-xl text-cyan-700 p-2">Email</label>
-                                <input type="email" className="input input-bordered w-full" placeholder="Email" />
-                            </div>
-                            <div>
-                                <label className="label text-cyan-700 text-xl p-2">Password</label>
-                                <input type="password" className="input input-bordered w-full" placeholder="Password" />
-                            </div>
-                            <div className="text-right">
-                                <a className="link link-hover text-sm">Forgot password?</a>
-                            </div>
-                            <button className="btn btn-ghost text-white text-xl font-semibold bg-cyan-700 w-full">Login</button>
-                            <p className="text-center font-semibold">
-                                Don't Have An Account ? <span className="text-green-700 font-bold"><Link to='/auth/signup'>SignUp</Link></span> First
-                            </p>
-                        </fieldset>
-                    </div>
+                    <form onSubmit={handleSubmit2}>
+                        <div className="card-body">
+                            <fieldset className="fieldset space-y-4">
+                                <div>
+                                    <label className="label text-xl text-cyan-700 p-2">Email</label>
+                                    <input name="email" type="email" className="input input-bordered w-full" placeholder="Email" />
+                                </div>
+                                <div>
+                                    <label className="label text-cyan-700 text-xl p-2">Password</label>
+                                    <input name="password" type="password" className="input input-bordered w-full" placeholder="Password" />
+                                </div>
+                                <div className="text-right">
+                                    <a className="link link-hover text-sm">Forgot password?</a>
+                                </div>
+                                <button className="btn btn-ghost text-white text-xl font-semibold bg-cyan-700 w-full">Login</button>
+                                <p className="text-center font-semibold">
+                                    Don't Have An Account ? <span className="text-green-700 font-bold"><Link to='/auth/signup'>SignUp</Link></span> First
+                                </p>
+                            </fieldset>
+                        </div>
+                    </form>
                 </div>
             </div>
 
